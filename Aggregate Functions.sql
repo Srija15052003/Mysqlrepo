ff707dc3d5a8dea4a4c8c5f7c11137ffa25cb3a5
Srija15052003 <168894684+Srija15052003@users.noreply.github.com>
2024-07-11 08:58:02 +0530

    Add files via upload

---
+++ Aggregate Functions.sql
@@ -0,0 +1,5 @@
+#SELECT COUNT(Percentage) FROM sbya.countrylanguage WHERE Percentage > 90.0
+#SELECT SUM(Percentage) FROM sbya.countrylanguage
+#SELECT Avg(Percentage) FROM sbya.countrylanguage
+#SELECT Max(Percentage) FROM sbya.countrylanguage
+SELECT Min(Percentage) FROM sbya.countrylanguage
